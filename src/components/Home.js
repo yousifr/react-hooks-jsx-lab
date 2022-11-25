a7cbd4fc372bf48ba5ffc9baaf3f5ba09155ec8b
yousif <yousifr.ismail@gmail.com>
2022-11-25 22:02:44 +0300

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        Your {name} is a Web Developer from Your {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
